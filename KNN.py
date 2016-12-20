a253eec1b7d6d30ea9e3bdb57583fa4f6a0ecc39
hanekensama <hanekensama@gmail.com>
2016-12-20 18:24:05 +0900

    finished to write basic functions

--- KNN.py
+++ KNN.py
@@ -1,33 +1,90 @@
+from collections import Counter
+from  math import hypot
+
+
 class KNN:
     """ K近傍法のためのノードを集めた集合
         add: ノードの追加
         cluster: 指定したノードのクラスタの番号を取得
-        nodes: 指定したクラスタのノードのリストを取得
+        dataList: 指定したクラスタのノードのリストを取得
     """
 
     def __init__(self):
         self.nodes = []
 
-    def add(self, value, cluster=None):
-        """ ノードの追加"""
+    def add(self, value, k=3, cluster=None):
+        """ ノードの追加
+            最初の学習時にはclusterに値を入れる
+            学習後に新たにノードを追加する場合はclusterの値はなし
+            :param value: データの値
+            :param int k: k近傍で用いるkの値
+            :param cluster: クラスタの識別子
+            :return: 新たに入れたデータのノード
+        """
         if cluster is None:
-            pass
+            cluster = self._checkCluster(value, k)
+
+        self.nodes.append((value, cluster))
+        return cluster
+
+    def cluster(self, value, k=3):
+        """ 指定したノードのクラスタを調べる
+            :param value: ノードの値
+            :param int k: k近傍で用いるkの値
+            :return: クラスタの識別子
+        """
+        if value in self.nodes:
+            return self._search(value)[1]
 
         else:
-            pass
+            return self._checkCluster(value, k)
 
-    def cluster(self, value):
-        """ 指定したノードのクラスタの番号を取得
-            :param value ノードの値
-        """
-        if not value in self.nodes:
-            return None
-
-        else
-            self._search(value):
-            pass
-
-    def nodes(self, cluster):
+    def getNodes(self, cluster):
         """ 指定したクラスタのノードのリストを取得
             :param cluster クラスタ
+            :return: ノードのリスト
+            :rtype: list of [Node.value, ... ]
         """
+        list = []
+
+        for node in self.nodes:
+            if node[1] == cluster:
+                list.append(node[0])
+
+        return list
+
+    def _search(self, value):
+        """ 指定された値のノードを探索する
+            O(n)
+            :param value: ノードの値
+            :return: ノードが見つかった場合はそのノード、見つからなければNone
+            :rtype: tuple of (value, cluster) or None
+        """
+        # TODO 探索の計算量を減らす.
+        for node in self.nodes:
+            if node[0] == value:
+                return node
+
+        return None
+
+    def _checkCluster(self, value, k):
+        """ 指定された値のノードのクラスタをユークリッド距離によるk近傍法にて求める
+            O(n) ただし、nはこれまでに学習したデータ量
+            :param value: ノードの値
+            :return: ノードが含まれるクラスタ
+            :rtype: cluster
+        """
+        dists = []
+        for node in self.nodes:
+            dx = node[0][0] - value[0]
+            dy = node[0][1] - value[1]
+            dists.append((hypot(dx, dy), node[1]))
+
+        dists.sort()
+        counter = Counter()
+        for i in range(min(k, len(dists))):
+            counter[dists[1]] += 1
+
+        most_common = counter.most_common(1)[0]
+        cluster = most_common[0][1]
+        return cluster
---
+++ main.py
@@ -0,0 +1,15 @@
+from KNN import KNN
+
+
+def main():
+    knn = KNN()
+    values = [(1, 2), (1, 3), (1, 1), (4, 4), (4, 5), (4, 6)]
+    clusters = ["red", "red", "red", "blue", "blue", "blue"]
+    for i in range(len(values)):
+        knn.add(value=values[i], cluster=clusters[i])
+
+    print(knn.add(value=(2, 1), k=5))
+
+
+if __name__ == '__main__':
+    main()
